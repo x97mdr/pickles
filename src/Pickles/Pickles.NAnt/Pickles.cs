8493ad49d988492e9517ec2041c32dddac272836
Jeffrey Cameron <jeffreycameron+github@gmail.com>
2011-10-22 08:03:42 -0400

    x97mdr/pickles#5 Added an NAnt task

---
+++ src/Pickles/Pickles.NAnt/Pickles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using Pickles.Formatters;
+
+namespace Pickles.NAnt
+{
+    public class Pickles : Task
+    {
+        [TaskAttribute("FeatureDirectory", Required = true)]
+        public string FeatureDirectory { get; set; }
+
+        [TaskAttribute("OutputDirectory", Required = true)]
+        public string OutputDirectory { get; set; }
+
+        protected override void ExecuteTask()
+        {
+            try
+            {
+                Project.Log(Level.Info, "Pickles v.{0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                Project.Log(Level.Info, "Reading features from {0}", FeatureDirectory ?? string.Empty);
+                Project.Log(Level.Info, "Writing output to {0}", OutputDirectory ?? string.Empty);
+
+                var documentFormatter = new HtmlDocumentFormatter(new HtmlTableOfContentsFormatter(), new HtmlFeatureFormatter(new HtmlScenarioFormatter(new HtmlStepFormatter(new HtmlTableFormatter(), new HtmlMultilineStringFormatter()))), new HtmlFooterFormatter());
+                var documentationBuilder = new HtmlDocumentationBuilder(new FeatureCrawler(new FeatureParser()), documentFormatter, new StylesheetWriter());
+
+                var featureDirectoryInfo = new DirectoryInfo(FeatureDirectory);
+                var OutputDirectoryInfo = new DirectoryInfo(OutputDirectory);
+
+                documentationBuilder.Build(featureDirectoryInfo, OutputDirectoryInfo);
+            }
+            catch (Exception e)
+            {
+                Project.Log(Level.Warning, e.Message);
+            }
+        }
+    }
+}
