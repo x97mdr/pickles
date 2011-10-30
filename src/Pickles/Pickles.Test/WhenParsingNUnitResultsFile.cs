6fe85d432de1f1faadeb97f6db450ed92e8a1780
Jeffrey Cameron <jeffreycameron+github@gmail.com>
2011-10-30 18:09:44 -0400

    Added code to be able to parse a results file and allow the user to determine if the test was run successfully or not

---
+++ src/Pickles/Pickles.Test/WhenParsingNUnitResultsFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using Pickles.TestFrameworks;
+
+namespace Pickles.Test
+{
+    [TestFixture]
+    public class WhenParsingNUnitResultsFile : BaseFixture
+    {
+        [Test]
+        public void Then_can_parse_simple_result_file_successfully()
+        {
+            var resultsFile = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Pickles.Test.results-example.xml");
+            var results = new Results();
+            results.Load(resultsFile);
+
+            Assert.AreEqual(true, results.GetFeatureResult("This Is A Feature").WasExecuted);
+            Assert.AreEqual(true, results.GetFeatureResult("This Is A Feature").IsSuccessful);
+
+            Assert.AreEqual(true, results.GetScenarioResult("The First Scenario").WasExecuted);
+            Assert.AreEqual(true, results.GetScenarioResult("The First Scenario").IsSuccessful);
+
+            Assert.AreEqual(true, results.GetScenarioResult("The Second Scenario").WasExecuted);
+            Assert.AreEqual(true, results.GetScenarioResult("The Second Scenario").IsSuccessful);
+
+            Assert.AreEqual(true, results.GetScenarioResult("The Third Scenario").WasExecuted);
+            Assert.AreEqual(true, results.GetScenarioResult("The Third Scenario").IsSuccessful);
+
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "THIS", "THAT" }).WasExecuted);
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "THIS", "THAT" }).IsSuccessful);
+
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "THE", "OTHER" }).WasExecuted);
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "THE", "OTHER" }).IsSuccessful);
+
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "THING", "LORUS" }).WasExecuted);
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "THING", "LORUS" }).IsSuccessful);
+
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "IPSUM", "DECOR" }).WasExecuted);
+            Assert.AreEqual(true, results.GetExampleResult("A Scenario Outline", new string[] { "IPSUM", "DECOR" }).IsSuccessful);
+        }
+    }
+}
---
+++ src/Pickles/Pickles/TestFrameworks/Results.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Pickles.TestFrameworks
+{
+    public struct TestResult
+    {
+        public bool IsSuccessful;
+        public bool WasExecuted;
+    }
+
+    public struct ExampleRowResult
+    {
+        public string[] RowValues;
+        public bool IsSuccessful;
+        public bool WasExecuted;
+    }
+
+    public class Results
+    {
+        private readonly Dictionary<string, TestResult> featureResults;
+        private readonly Dictionary<string, TestResult> scenarioResults;
+        private readonly Dictionary<string, List<ExampleRowResult>> exampleResults;
+
+        public Results()
+        {
+            this.featureResults = new Dictionary<string, TestResult>();
+            this.scenarioResults = new Dictionary<string, TestResult>();
+            this.exampleResults = new Dictionary<string, List<ExampleRowResult>>();
+        }
+
+        private string[] ExtractRowValuesFromName(string exampleName)
+        {
+            var parts = exampleName.Split(new char[] { '(', ')', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var values = new List<string>();
+            values.AddRange(parts.Skip(1).Where(x => x != "System.String[]").Select(x => x.Replace("\"", "")));
+            return values.ToArray();
+        }
+
+        private bool IsRowMatched(string[] a, string[] b)
+        {
+            if (a.Length != b.Length) return false;
+            for (int index = 0; index < a.Length; index++)
+            {
+                if (a[index] != b[index]) return false;
+            }
+            return true;
+        }
+
+        public void Load(string filePath)
+        {
+            Load(new FileInfo(filePath));
+        }
+
+        public void Load(FileInfo file)
+        {
+            using (var stream = file.OpenRead())
+            {
+                Load(stream);
+            }
+        }
+
+        public void Load(Stream stream)
+        {
+            var resultsDocument = XDocument.Load(stream);
+
+            var featureElements = from element in resultsDocument.Root.Descendants()
+                                  where element.Name == ("test-suite")
+                                     && element.Attribute("description") != null
+                                     && !element.Element("results").Elements().All(resultChild => resultChild.Name.LocalName == "test-case")
+                                  select element;
+
+            foreach (var featureElement in featureElements)
+            {
+                this.featureResults.Add(featureElement.Attribute("description").Value, new TestResult
+                {
+                    IsSuccessful = featureElement.Attribute("success").Value.ToLowerInvariant() == "true",
+                    WasExecuted = featureElement.Attribute("executed").Value.ToLowerInvariant() == "true"
+                });
+
+                var scenarioElements = featureElement.Element("results").Elements("test-case");
+                foreach (var scenarioElement in scenarioElements)
+                {
+                    this.scenarioResults.Add(scenarioElement.Attribute("description").Value, new TestResult
+                    {
+                        IsSuccessful = scenarioElement.Attribute("success").Value.ToLowerInvariant() == "true",
+                        WasExecuted = scenarioElement.Attribute("executed").Value.ToLowerInvariant() == "true"
+                    });
+                }
+
+                var scenarioOutlineElements = featureElement.Element("results").Elements("test-suite");
+                foreach (var scenarioOutlineElement in scenarioOutlineElements)
+                {
+                    var examples = scenarioOutlineElement.Element("results").Elements("test-case");
+                    var exampleResults = new List<ExampleRowResult>();
+                    foreach (var example in examples)
+                    {
+                        exampleResults.Add(new ExampleRowResult
+                        {
+                            RowValues = ExtractRowValuesFromName(example.Attribute("name").Value),
+                            IsSuccessful = scenarioOutlineElement.Attribute("success").Value.ToLowerInvariant() == "true",
+                            WasExecuted = scenarioOutlineElement.Attribute("executed").Value.ToLowerInvariant() == "true"
+                        });
+                    }
+                    this.exampleResults.Add(scenarioOutlineElement.Attribute("description").Value, exampleResults);
+                }
+
+            }
+        }
+
+        public TestResult GetFeatureResult(string name)
+        {
+            TestResult testResult;
+            if (this.featureResults.TryGetValue(name, out testResult))
+            {
+                return testResult;
+            }
+
+            throw new InvalidOperationException("Cannot retrieve test results for feature named " + name);
+        }
+
+        public TestResult GetScenarioResult(string name)
+        {
+            TestResult testResult;
+            if (this.scenarioResults.TryGetValue(name, out testResult))
+            {
+                return testResult;
+            }
+
+            throw new InvalidOperationException("Cannot retrieve test results for scenario named " + name);
+        }
+
+        public TestResult GetExampleResult(string name, string[] row)
+        {
+            List<ExampleRowResult> exampleRowResults;
+            if (this.exampleResults.TryGetValue(name, out exampleRowResults))
+            {
+                var result = exampleRowResults.Single(x => IsRowMatched(row, x.RowValues));
+                return new TestResult
+                {
+                    IsSuccessful = result.IsSuccessful,
+                    WasExecuted = result.WasExecuted
+                };
+            }
+
+            throw new InvalidOperationException("Cannot retrieve test results for scenario named " + name);
+        }
+    }
+}
