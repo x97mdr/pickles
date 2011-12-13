3e2b3daec7f2beaeff4dfb23c47da4137ac65fb6
Jeffrey Cameron <jeffreycameron+github@gmail.com>
2011-12-12 19:28:56 -0500

    #48 Added package creation to the build script and #20 added ILMerge support for all outputs to the build script

--- NuGetPackAll.cmd
+++ NuGetPackAll.cmd
@@ -1,10 +1,8 @@
-cd src\Pickles
-cd Pickles
-call NugetPack.cmd %1
-cd ..\Pickles.CommandLine
-call NugetPack.cmd
-cd ..\Pickles.MsBuild
-call NugetPack.cmd
-cd ..\Pickles.NAnt
-call NugetPack.cmd
-cd \..\..
+pushd src\Pickles\Pickles.CommandLine && ..\.nuget\nuget.exe pack Pickles.CommandLine.csproj -Properties Configuration=Release
+popd
+pushd src\Pickles\Pickles.MSBuild && ..\.nuget\nuget.exe pack Pickles.MSBuild.csproj -Properties Configuration=Release 
+popd
+pushd src\Pickles\Pickles.NAnt && ..\.nuget\nuget.exe pack Pickles.NAnt.csproj -Properties Configuration=Release 
+popd
+pushd src\Pickles\Pickles.PowerShell && ..\.nuget\nuget.exe pack Pickles.nuspec -Version 0.3
+popd
