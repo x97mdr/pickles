ce65aacf1e71a753bcd6f7332d780d9699219bbf
Jeffrey Cameron <jeffreycameron@gmail.com>
2012-01-01 09:52:48 -0500

    #24 Apparently the TestResult.cs file was not committed because of an ignore rule

---
+++ src/Pickles/Pickles/TestFrameworks/TestResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pickles.TestFrameworks
+{
+    public struct TestResult
+    {
+        public bool WasExecuted;
+        public bool WasSuccessful;
+    }
+}
