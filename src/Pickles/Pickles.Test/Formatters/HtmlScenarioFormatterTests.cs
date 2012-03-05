0ecdc127f6ba423e1aabf0b61e54caefc95705e7
Dirk Rombauts <dirk.rombauts@questmaster.net>
2012-03-05 13:32:56 +0100

    Failing test for no id attribute on li items in scenario.

---
+++ src/Pickles/Pickles.Test/Formatters/HtmlScenarioFormatterTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Ninject;
+using Pickles.DocumentationBuilders.HTML;
+using Pickles.Parser;
+
+namespace Pickles.Test.Formatters
+{
+  [TestFixture]
+  public class HtmlScenarioFormatterTests : BaseFixture
+  {
+    private HtmlScenarioFormatter formatter;
+
+     [SetUp]
+    public void Setup()
+     {
+       this.formatter = new HtmlScenarioFormatter(
+         Kernel.Get<HtmlStepFormatter>(),
+         Kernel.Get<HtmlDescriptionFormatter>(),
+         Kernel.Get<HtmlImageResultFormatter>());
+     }
+
+     [Test]
+     public void Li_Element_Must_Not_Have_Id_Attribute()
+     {
+       var scenario = BuildMinimalScenario();
+
+       XElement li = formatter.Format(scenario, 1);
+
+       var idAttribute = li.Attribute("id");
+
+       Assert.IsNull(idAttribute);
+     }
+
+    private Scenario BuildMinimalScenario()
+    {
+      return new Scenario
+               {
+                 Description = "My Scenario Description",
+                 Steps = new List<Step>
+                           {
+                             new Step
+                               {
+                                 NativeKeyword = "Given",
+                                 Name = "My Step Name",
+                               }
+                           }
+               };
+    }
+  }
+}
