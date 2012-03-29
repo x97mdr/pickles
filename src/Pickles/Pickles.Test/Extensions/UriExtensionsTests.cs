e5e6c60b228653e3dc92ce0f97197c9a07490132
Dirk Rombauts <dirk.rombauts@questmaster.net>
2012-03-29 08:17:49 +0200

    Added tests and implementation of UriExtensions.

---
+++ src/Pickles/Pickles.Test/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Pickles.Extensions;
+
+namespace Pickles.Test.Extensions
+{
+  [TestFixture]
+  public class UriExtensionsTests
+  {
+    [Test]
+    public void DirectoryInfo_WithTrailingSlash_ProducesUriWithTrailingSlash()
+    {
+      var directoryInfo = new DirectoryInfo(@"c:\temp\");
+
+      Uri uri = directoryInfo.ToUri();
+
+      Assert.AreEqual("file:///c:/temp/", uri.ToString());
+    }
+
+    [Test]
+    public void DirectoryInfo_WithoutTrailingSlash_ProducesUriWithTrailingSlash()
+    {
+      var directoryInfo = new DirectoryInfo(@"c:\temp");
+
+      Uri uri = directoryInfo.ToUri();
+
+      Assert.AreEqual("file:///c:/temp/", uri.ToString());
+    }
+
+    [Test]
+    public void FileSystemInfo_DirectoryWithTrailingSlash_ProducesUriWithTrailingSlash()
+    {
+      FileSystemInfo fsi = new DirectoryInfo(@"c:\temp\");
+
+      Uri uri = fsi.ToUri();
+
+      Assert.AreEqual("file:///c:/temp/", uri.ToString());
+    }
+
+    [Test]
+    public void FileInfo_NormalFilename_ProducesUri()
+    {
+      FileInfo fileInfo = new FileInfo(@"c:\temp\test.txt");
+
+      Uri uri = fileInfo.ToUri();
+
+      Assert.AreEqual("file:///c:/temp/test.txt", uri.ToString());
+    }
+
+    [Test]
+    public void FileSystemInfo_FileInfo_ProducesUri()
+    {
+      FileSystemInfo fsi = new FileInfo(@"c:\temp\test.txt");
+
+      Uri uri = fsi.ToUri();
+
+      Assert.AreEqual("file:///c:/temp/test.txt", uri.ToString());
+    }
+  }
+}
---
+++ src/Pickles/Pickles/Extensions/UriExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Pickles.Extensions
+{
+  public static class UriExtensions
+  {
+     public static Uri ToUri(this DirectoryInfo instance)
+     {
+       string fullName = instance.FullName;
+
+       if (!instance.FullName.EndsWith(@"\"))
+       {
+         fullName = fullName + @"\";
+       }
+
+       return new Uri(fullName);
+     }
+
+     public static Uri ToUri(this FileSystemInfo instance)
+     {
+       DirectoryInfo di = instance as DirectoryInfo;
+
+       if (di != null)
+       {
+         return ToUri(di);
+       }
+       
+       return ToUri((FileInfo)instance);
+     }
+
+    public static Uri ToUri(this FileInfo instance)
+     {
+       return new Uri(instance.FullName);
+     }
+  }
+}
