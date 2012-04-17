aa2880be680240873761c6bc5a48575a5a1b290a
Niklas Nihlen <niklas.nihlen@gmail.com>
2012-04-17 17:35:30 +0200

    Added more tests for feature test result parsing - especially for failing features. Led me to fixa a bug in MsTestReslut.GetFeatureReslut wich always returned true

--- src/Pickles/Pickles.Test/Formatters/JSON/when_creating_a_feature_with_meta_info_and_test_result_in_mstest_format.cs
+++ src/Pickles/Pickles.Test/Formatters/JSON/when_creating_a_feature_with_meta_info_and_test_result_in_mstest_format.cs
@@ -16,10 +16,10 @@ namespace Pickles.Test.Formatters.JSON
 {
   public class when_creating_a_feature_with_meta_info_and_test_result_in_mstest_format : BaseFixture
   {
-    private const string ROOT_PATH = @"FakeFolderStructures";
+    private const string ROOT_PATH = @"Formatters\JSON\Features";
     private const string OUTPUT_DIRECTORY = @"JSONFeatureOutput";
     private string filePath = Path.Combine(OUTPUT_DIRECTORY, JSONDocumentationBuilder.JS_FILE_NAME);
-    private string testResultFilePath = @"results-example-mstest.trx";
+    private string testResultFilePath = @"Formatters\JSON\results-example-failing-and-pasing-mstest.trx";
 
     [TestFixtureSetUp]
     public void Setup()
@@ -71,17 +71,75 @@ namespace Pickles.Test.Formatters.JSON
     }
 
     [Test]
-    public void it_should_contain_WasExecuted_key_with_value_True_in_the_json_document()
+    public void it_should_indicate_WasSuccessful_is_true()
     {
       var content = File.ReadAllText(filePath);
-      JArray jsonObject = JArray.Parse(content);
+      JArray jsonArray = JArray.Parse(content);
 
-      Assert.AreEqual("True", jsonObject[0]["Result"]["WasExecuted"].ToString());
+    
+      var featureJsonElement = from feat in jsonArray
+                where feat["Feature"]["Name"].Value<string>().Equals("Two more scenarios transfering funds between accounts")
+                select feat;
+
+      Assert.IsTrue( featureJsonElement.ElementAt(0)["Result"]["WasSuccessful"].Value<bool>());
+    }
+
+    [Test]
+    public void it_should_indicate_WasSuccessful_is_true_for_the_other_success_feature()
+    {
+      var content = File.ReadAllText(filePath);
+      JArray jsonArray = JArray.Parse(content);
+
+
+      var featureJsonElement = from feat in jsonArray
+                               where feat["Feature"]["Name"].Value<string>().Equals("Transfer funds between accounts")
+                               select feat;
+
+      Assert.IsTrue(featureJsonElement.ElementAt(0)["Result"]["WasSuccessful"].Value<bool>());
+    }
+
+    [Test]
+    public void it_should_indicate_WasSuccessful_is_false_for_failing_scenario()
+    {
+      var content = File.ReadAllText(filePath);
+      JArray jsonArray = JArray.Parse(content);
+
+
+      var featureJsonElement = from feat in jsonArray
+                               where feat["Feature"]["Name"].Value<string>().Equals("Transfer funds between accounts onc scenario and FAILING")
+                               select feat;
+
+      Assert.IsFalse(featureJsonElement.ElementAt(0)["Result"]["WasSuccessful"].Value<bool>());
+    }
+
+
+    [Test]
+    public void it_should_indicate_WasSuccessful_is_false_for_another_failing_scenario()
+    {
+      var content = File.ReadAllText(filePath);
+      JArray jsonArray = JArray.Parse(content);
+
+
+      var featureJsonElement = from feat in jsonArray
+                               where feat["Feature"]["Name"].Value<string>().Equals("Two more scenarios transfering funds between accounts - one failng and one succeding")
+                               select feat;
+
+      Assert.IsFalse(featureJsonElement.ElementAt(0)["Result"]["WasSuccessful"].Value<bool>());
     }
 
 
     [Test]
     public void it_should_contain_WasSuccessful_key_in_Json_document()
+    {
+      string content = File.ReadAllText(filePath);
+      JArray jsonArray = JArray.Parse(content);
+
+      Assert.IsNotEmpty(jsonArray[0]["Result"]["WasSuccessful"].ToString());
+    }
+
+
+    [Test]
+    public void it_should_WasSuccessful_false_for_feature_X_Json_document()
     {
       string content = File.ReadAllText(filePath);
       JArray jsonArray = JArray.Parse(content);
