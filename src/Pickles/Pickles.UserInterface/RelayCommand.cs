c03fdb540b491221b773e3c5121b46eac8a83b65
Dirk Rombauts <dirk.rombauts@questmaster.net>
2012-07-17 13:20:32 +0200

    More options in the relaycommand

--- src/Pickles/Pickles.UserInterface/RelayCommand.cs
+++ src/Pickles/Pickles.UserInterface/RelayCommand.cs
@@ -1,44 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Input;
 
 namespace Pickles.UserInterface
 {
-    public class RelayCommand : ICommand
+  public class RelayCommand : ICommand
+  {
+    private readonly Action<object> execute;
+
+    private readonly Func<object, bool> canExecute;
+
+    public RelayCommand(Action execute)
+      : this(o => execute())
     {
-        private readonly Action execute;
-
-        public RelayCommand(Action execute)
-        {
-            this.execute = execute;
-        }
-
-        #region Implementation of ICommand
-
-        public void Execute(object parameter)
-        {
-            execute();
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return execute != null;
-        }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add
-            {
-                if (CanExecute(null)) CommandManager.RequerySuggested += value;
-            }
-            remove
-            {
-                if (CanExecute(null)) CommandManager.RequerySuggested -= value;
-            }
-        }
-
-        #endregion
     }
+
+    public RelayCommand(Action execute, Func<bool> canExecute)
+      : this(o => execute(), o => canExecute())
+    {
+    }
+
+    public RelayCommand(Action<object> execute)
+      : this(execute, o => true)
+    {
+    }
+
+    public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+    {
+      this.execute = execute;
+      this.canExecute = canExecute;
+    }
+
+    #region Implementation of ICommand
+
+    public void Execute(object parameter)
+    {
+      execute(parameter);
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return canExecute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    #endregion
+
+    public void RaiseCanExecuteChanged()
+    {
+      this.CanExecuteChanged.Raise(this, EventArgs.Empty);
+    }
+  }
 }
