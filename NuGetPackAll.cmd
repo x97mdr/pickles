0c5127b61a131d796e71a4d756f27ed03b73889e
Jeffrey Cameron <jeffreycameron@gmail.com>
2012-08-07 12:04:21 -0400

    Set CopyLocal=False on all unnecessary IKVM assemblies

--- NuGetPackAll.cmd
+++ NuGetPackAll.cmd
@@ -1,8 +1,2 @@
-pushd src\Pickles\Pickles.CommandLine && ..\.nuget\nuget.exe pack Pickles.CommandLine.csproj -Properties Configuration=Release
-popd
-pushd src\Pickles\Pickles.MSBuild && ..\.nuget\nuget.exe pack Pickles.MSBuild.csproj -Properties Configuration=Release 
-popd
-pushd src\Pickles\Pickles.NAnt && ..\.nuget\nuget.exe pack Pickles.NAnt.csproj -Properties Configuration=Release 
-popd
-pushd src\Pickles\Pickles.PowerShell && ..\.nuget\nuget.exe pack Pickles.nuspec -Version 0.3
+pushd src\Pickles\Pickles.PowerShell && ..\.nuget\nuget.exe pack Pickles.nuspec -Version %1
 popd
