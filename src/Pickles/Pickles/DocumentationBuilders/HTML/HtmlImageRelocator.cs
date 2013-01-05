716a909815ec1ebfe6845498509c0721f5fad95e
Jeffrey Cameron <jeffreycameron@gmail.com>
2013-01-05 15:43:08 -0500

    #12 Initial support for moving images
    Added support for moving images based on relative paths.  Test included.
    Tests need to be expanded to handle deeper hierarchies and absolute
    paths in the future.

---
+++ src/Pickles/Pickles/DocumentationBuilders/HTML/HtmlImageRelocator.cs
@@ -0,0 +1,38 @@
+﻿using PicklesDoc.Pickles.DirectoryCrawler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace PicklesDoc.Pickles.DocumentationBuilders.HTML
+{
+    public class HtmlImageRelocator
+    {
+        private readonly Configuration configuration;
+
+        public HtmlImageRelocator(Configuration configuration)
+        {
+            this.configuration = configuration;
+        }
+        
+        public void Relocate(FeatureDirectoryTreeNode node, XElement parsedFeature)
+        {
+            var images = parsedFeature.Descendants(HtmlNamespace.Xhtml + "img");
+
+            foreach (var image in images)
+            {
+                var sourceValue = image.Attribute("src").Value;
+
+                if (!String.IsNullOrEmpty(sourceValue))
+                {
+                    var relativePathToImage = Path.Combine(Path.GetDirectoryName(node.RelativePathFromRoot), sourceValue);
+                    var source = Path.Combine(configuration.FeatureFolder.FullName, relativePathToImage);
+                    var destination = Path.Combine(configuration.OutputFolder.FullName, relativePathToImage);
+                    File.WriteAllBytes(destination, File.ReadAllBytes(source));
+                }
+            }
+        }
+    }
+}
