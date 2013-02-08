49e0602ba0509ed6c8c01233f2bf8a53ebfdec9e
Dirk Rombauts <dirk.rombauts@questmaster.net>
2013-02-08 09:26:53 +0100

    Making better use of space in UI

--- src/Pickles/Pickles.UserInterface/MainWindow.xaml.cs
+++ src/Pickles/Pickles.UserInterface/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Shell;
 
 namespace PicklesDoc.Pickles.UserInterface
 {
@@ -17,7 +20,7 @@ namespace PicklesDoc.Pickles.UserInterface
             get { return this.DataContext as MainWindowViewModel; }
         }
 
-        private void MetroWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
             this.ViewModel.LoadFromSettings();
         }
@@ -25,6 +28,36 @@ namespace PicklesDoc.Pickles.UserInterface
         private void MetroWindow_Closed(object sender, EventArgs e)
         {
             this.ViewModel.SaveToSettings();
+        }
+
+        private void MainWindow_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+          var oldVm = e.NewValue as MainWindowViewModel;
+
+          if (oldVm != null)
+          {
+            oldVm.PropertyChanged -= this.ViewModelOnPropertyChanged;
+          }
+
+          var newVm = e.NewValue as MainWindowViewModel;
+
+          if (newVm != null)
+          {
+            newVm.PropertyChanged += this.ViewModelOnPropertyChanged;
+          }
+        }
+
+        private void ViewModelOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+          switch (propertyChangedEventArgs.PropertyName)
+          {
+            case "IsRunning":
+              {
+                this.progressIndicator.Visibility = this.ViewModel.IsRunning ? Visibility.Visible : Visibility.Hidden;
+                this.taskBarItemInfo.ProgressState = this.ViewModel.IsRunning ? TaskbarItemProgressState.Indeterminate : TaskbarItemProgressState.None;
+                break;
+              }
+          }
         }
     }
 }
