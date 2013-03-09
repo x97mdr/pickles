671f233b6fc39e76501bc669b12f9d284530b6fe
Will Green <will@hotgazpacho.org>
2013-03-08 23:07:09 -0500

    Update NuGetPackAll.cmd to create MSBuild package

--- NuGetPackAll.cmd
+++ NuGetPackAll.cmd
@@ -1,2 +1,4 @@
 pushd src\Pickles\Pickles.PowerShell && ..\.nuget\nuget.exe pack Pickles.nuspec -Version %1
 popd
+pushd src\Pickles\Pickles.MSBuild && ..\.nuget\nuget.exe pack Pickles.MSBuild.nuspec -Version %1
+popd
