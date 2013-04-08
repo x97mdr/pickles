3c63a99decbec1baf66ae7b87c9a6c2b69bb2aa7
Dirk Rombauts <dirk.rombauts@questmaster.net>
2013-04-08 10:41:01 +0200

    Pickles.Tests Passes

--- src/Pickles/Pickles.Test/Formatters/HtmlContentFormatterTests.cs
+++ src/Pickles/Pickles.Test/Formatters/HtmlContentFormatterTests.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.IO;
+using System.Xml.Linq;
+using Autofac;
 using Moq;
 using NUnit.Framework;
-using Autofac;
 using PicklesDoc.Pickles.DirectoryCrawler;
 using PicklesDoc.Pickles.DocumentationBuilders.HTML;
 using PicklesDoc.Pickles.Parser;
-using System.Xml.Linq;
 
 namespace PicklesDoc.Pickles.Test.Formatters
 {
@@ -42,7 +41,7 @@ namespace PicklesDoc.Pickles.Test.Formatters
         public void Format_ContentIsFeatureNode_UsesHtmlFeatureFormatterWithCorrectArgument()
         {
             var fakeHtmlFeatureFormatter = new Mock<IHtmlFeatureFormatter>();
-            var fakeHtmlImageRelocator = new Mock<HtmlImageRelocator>(null);
+            var fakeHtmlImageRelocator = new Mock<HtmlImageRelocator>(null, null);
             fakeHtmlImageRelocator.Setup(x => x.Relocate(It.IsAny<INode>(), It.IsAny<XElement>()));
             var formatter = new HtmlContentFormatter(fakeHtmlFeatureFormatter.Object, Container.Resolve<HtmlIndexFormatter>(), fakeHtmlImageRelocator.Object);
 
