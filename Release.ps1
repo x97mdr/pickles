6cc069302175ee7d323a0f84054acc662307c097
Dirk Rombauts <dirk.rombauts@questmaster.net>
2013-09-27 09:06:56 +0200

    Beginning of automating result generation in release script

--- Release.ps1
+++ Release.ps1
@@ -11,3 +11,7 @@ Move-Item .\Pickles.MSBuild.$version.nupkg deploy -Force
 $out = .\src\Pickles\.nuget\nuget.exe pack .\src\Pickles\Pickles.CommandLine\Pickles.CommandLine.nuspec -Version $version
 Write-Host $out
 Move-Item .\Pickles.CommandLine.$version.nupkg deploy -Force
+
+# New-Item -ItemType directory -Path .\deploy\Html -Force
+# $out = .\src\Pickles\Pickles.CommandLine\bin\Debug\Pickles.exe -f=.\src\Pickles\Examples\Features\ -o=.\deploy\Html --df=Html
+# Write-Host $out
