ad16215f8aa4215e8d93c75f3a430fe5b009ecdd
Dirk Rombauts <dirk.rombauts@questmaster.net>
2013-10-28 09:02:04 +0100

    Downgrade to version 0.12.1 to publish a bugfix

--- Release.ps1
+++ Release.ps1
@@ -1,8 +1,8 @@
+$version = "0.12.1"
+
 git pull origin master
 
 start-process ".\tools\nant-0.85\binaries\nant.exe" "-buildfile:project.build pack" -Wait
-
-$version = "0.13.0"
 
 $out = .\src\Pickles\.nuget\nuget.exe pack .\src\Pickles\Pickles.MSBuild\Pickles.MSBuild.nuspec -Version $version
 Write-Host $out
@@ -11,6 +11,12 @@ Move-Item .\Pickles.MSBuild.$version.nupkg deploy -Force
 $out = .\src\Pickles\.nuget\nuget.exe pack .\src\Pickles\Pickles.CommandLine\Pickles.CommandLine.nuspec -Version $version
 Write-Host $out
 Move-Item .\Pickles.CommandLine.$version.nupkg deploy -Force
+
+Copy-Item .\src\Pickles\Pickles.CommandLine\bin\Release -Destination .\deploy\pickles-$version\cmd -Recurse -Force
+Copy-Item .\src\Pickles\Pickles.MSBuild\bin\Release -Destination .\deploy\pickles-$version\msbuild -Recurse -Force
+Copy-Item .\src\Pickles\Pickles.NAnt\bin\Release -Destination .\deploy\pickles-$version\nant -Recurse -Force
+Copy-Item .\src\Pickles\Pickles.PowerShell\bin\Release -Destination .\deploy\pickles-$version\powershell -Recurse -Force
+Copy-Item .\src\Pickles\Pickles.UserInterface\bin\Release -Destination .\deploy\pickles-$version\ui -Recurse -Force
 
 # New-Item -ItemType directory -Path .\deploy\Html -Force
 # $out = .\src\Pickles\Pickles.CommandLine\bin\Debug\Pickles.exe -f=.\src\Pickles\Examples\Features\ -o=.\deploy\Html --df=Html
