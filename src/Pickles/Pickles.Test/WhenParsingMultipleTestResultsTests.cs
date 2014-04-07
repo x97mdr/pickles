30ee11657869d90c026c341d3883abf8e08ea2af
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-04-07 16:30:31 +0200

    Tests for MultipleTestResults

---
+++ src/Pickles/Pickles.Test/WhenParsingMultipleTestResultsTests.cs
@@ -0,0 +1,92 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="WhenParsingMultipleTestResultsTests.cs" company="PicklesDoc">
+//  Copyright 2011 Jeffrey Cameron
+//  Copyright 2012-present PicklesDoc team and community contributors
+//  
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using Moq;
+
+using NUnit.Framework;
+
+using PicklesDoc.Pickles.Parser;
+using PicklesDoc.Pickles.TestFrameworks;
+
+using Should;
+
+namespace PicklesDoc.Pickles.Test
+{
+  [TestFixture]
+  public class WhenParsingMultipleTestResultsTests
+  {
+    [Test]
+    public void GetFeatureResult_OnePassingOneInconclusive_ReturnsPassed()
+    {
+      var feature = new Feature();
+
+      var testResults1 = SetupStubForGetFeatureResult(feature, TestResult.Passed);
+      var testResults2 = SetupStubForGetFeatureResult(feature, TestResult.Inconclusive);
+
+      ITestResults multipleTestResults = CreateMultipleTestResults(testResults1.Object, testResults2.Object);
+
+      var result = multipleTestResults.GetFeatureResult(feature);
+
+      result.ShouldEqual(TestResult.Passed);
+    }
+
+    private static MultipleTestResults CreateMultipleTestResults(ITestResults testResults1, ITestResults testResults2)
+    {
+      return new MultipleTestResults(new[] { testResults1, testResults2 });
+    }
+
+    private static Mock<ITestResults> SetupStubForGetFeatureResult(Feature feature, TestResult resultOfGetFeatureResult)
+    {
+      var testResults1 = new Mock<ITestResults>();
+      testResults1.Setup(ti => ti.GetFeatureResult(feature)).Returns(resultOfGetFeatureResult);
+      return testResults1;
+    }
+
+    [Test]
+    public void GetFeatureResult_OnePassingOneFailed_ReturnsPassed()
+    {
+      var feature = new Feature();
+
+      var testResults1 = SetupStubForGetFeatureResult(feature, TestResult.Passed);
+      var testResults2 = SetupStubForGetFeatureResult(feature, TestResult.Failed);
+
+      ITestResults multipleTestResults = CreateMultipleTestResults(testResults1.Object, testResults2.Object);
+
+      var result = multipleTestResults.GetFeatureResult(feature);
+
+      result.ShouldEqual(TestResult.Failed);
+    }
+
+    [Test]
+    public void GetFeatureResult_TwoInconclusive_ReturnsInconclusive()
+    {
+      var feature = new Feature();
+
+      var testResults1 = SetupStubForGetFeatureResult(feature, TestResult.Inconclusive);
+      var testResults2 = SetupStubForGetFeatureResult(feature, TestResult.Inconclusive);
+
+      ITestResults multipleTestResults = CreateMultipleTestResults(testResults1.Object, testResults2.Object);
+
+      var result = multipleTestResults.GetFeatureResult(feature);
+
+      result.ShouldEqual(TestResult.Inconclusive);
+    }
+  }
+}
---
+++ src/Pickles/Pickles/TestFrameworks/MultipleTestResults.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PicklesDoc.Pickles.Parser;
+
+namespace PicklesDoc.Pickles.TestFrameworks
+{
+  public class MultipleTestResults : ITestResults
+  {
+    private readonly IEnumerable<ITestResults> testResults;
+
+    public MultipleTestResults(IEnumerable<ITestResults> testResults)
+    {
+      this.testResults = testResults;
+    }
+
+    public TestResult GetFeatureResult(Feature feature)
+    {
+      var results = testResults.Select(tr => tr.GetFeatureResult(feature)).ToArray();
+
+      return EvaluateTestResults(results);
+    }
+
+    private static TestResult EvaluateTestResults(TestResult[] results)
+    {
+      if (results.Any(r => r == TestResult.Failed))
+      {
+        return TestResult.Failed;
+      }
+
+      if (results.Any(r => r == TestResult.Passed))
+      {
+        return TestResult.Passed;
+      }
+
+      return TestResult.Inconclusive;
+    }
+
+    public TestResult GetScenarioOutlineResult(ScenarioOutline scenarioOutline)
+    {
+      throw new NotImplementedException();
+    }
+
+    public TestResult GetScenarioResult(Scenario scenario)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
