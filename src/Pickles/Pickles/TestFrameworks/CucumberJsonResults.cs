d02cff4735a48fc4a7293c6d902a36dfa2bc2eb2
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-04-08 10:35:59 +0200

    CucumberJsonResults now supports multiple test results too

--- src/Pickles/Pickles/TestFrameworks/CucumberJsonResults.cs
+++ src/Pickles/Pickles/TestFrameworks/CucumberJsonResults.cs
@@ -1,95 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json;
-using PicklesDoc.Pickles.Parser;
-using  CucumberObjects = PicklesDoc.Pickles.Parser.JsonResult;
-
-using StreamReader = System.IO.StreamReader;
+using System.IO.Abstractions;
 
 namespace PicklesDoc.Pickles.TestFrameworks
 {
-    public class CucumberJsonResults : ITestResults
+    public class CucumberJsonResults : MultipleTestResults
     {
-        private readonly Configuration configuration;
-        private List<CucumberObjects.Feature> resultsDocument;
-
         public CucumberJsonResults(Configuration configuration)
+          : base(configuration)
         {
-            this.configuration = configuration;
-            if (configuration.HasTestResults)
-            {
-                this.resultsDocument = this.ReadResultsFile();
-            }
         }
 
-        private List<CucumberObjects.Feature> ReadResultsFile()
+        protected override ITestResults ConstructSingleTestResult(FileInfoBase fileInfo)
         {
-            List<CucumberObjects.Feature> result;
-            using (var stream = this.configuration.TestResultsFile.OpenRead())
-            {
-                using (var reader = new StreamReader(stream))
-                {
-                    result = JsonConvert.DeserializeObject<List<CucumberObjects.Feature>>(reader.ReadToEnd());
-                }
-            }
-            return result;
+          return new CucumberJsonSingleResults(fileInfo);
         }
-
-        #region ITestResults Members
-
-        public TestResult GetFeatureResult(Feature feature)
-        {
-            CucumberObjects.Feature cucumberFeature = this.GetFeatureElement(feature);
-            return this.GetResultFromFeature(cucumberFeature);
-
-        }
-
-        public TestResult GetScenarioOutlineResult(ScenarioOutline scenarioOutline)
-        {
-            //Not applicable
-            return new TestResult();
-        }
-
-        public TestResult GetScenarioResult(Scenario scenario)
-        {
-            CucumberObjects.Element cucumberScenario  = null;
-            CucumberObjects.Feature cucumberFeature = this.GetFeatureElement(scenario.Feature);
-            if(cucumberFeature != null)
-             cucumberScenario = cucumberFeature.elements.FirstOrDefault(x => x.name == scenario.Name);
-            return this.GetResultFromScenario(cucumberScenario);
-
-        }
-
-        #endregion
-
-        private CucumberObjects.Feature GetFeatureElement(Feature feature)
-        {
-            return this.resultsDocument.FirstOrDefault(x => x.name == feature.Name);
-        }
-
-        private TestResult GetResultFromScenario(CucumberObjects.Element cucumberScenario)
-        {
-            if (cucumberScenario == null) return TestResult.Inconclusive;
-
-            bool wasSuccessful = CheckScenarioStatus(cucumberScenario);
-
-            return wasSuccessful ? TestResult.Passed : TestResult.Failed;
-        }
-
-        private static bool CheckScenarioStatus(CucumberObjects.Element cucumberScenario)
-        {
-            return cucumberScenario.steps.All(x => x.result.status == "passed");
-        }
-
-        private TestResult GetResultFromFeature(CucumberObjects.Feature cucumberFeature)
-        {
-            if (cucumberFeature == null || cucumberFeature.elements == null) return TestResult.Inconclusive;
-
-            bool wasSuccessful = cucumberFeature.elements.All(CheckScenarioStatus);
-
-            return wasSuccessful ? TestResult.Passed : TestResult.Failed;
-        }
-
     }
 }
