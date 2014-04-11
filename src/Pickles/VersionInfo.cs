867d8720f0e7625d0ecf51fe44107fe8e7a50522
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-04-11 08:06:06 +0200

    Version bump (0.15.0)

--- src/Pickles/VersionInfo.cs
+++ src/Pickles/VersionInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.14.0.0")]
-[assembly: AssemblyFileVersion("0.14.0.0")]
+[assembly: AssemblyVersion("0.15.0.0")]
+[assembly: AssemblyFileVersion("0.15.0.0")]
