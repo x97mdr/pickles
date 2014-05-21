574be61125ee62b1c1f359dc1a8dc1a7eb85492f
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-05-21 09:05:05 +0200

    Add tests for scenario outline results

--- src/Pickles/Pickles.Test/WhenParsingxUnitResultsFile.cs
+++ src/Pickles/Pickles.Test/WhenParsingxUnitResultsFile.cs
@@ -119,5 +119,106 @@ namespace PicklesDoc.Pickles.Test
 
           Assert.Throws<InvalidOperationException>(() => results.GetExampleResult(scenarioOutline, new[] { "40", "50", "90" }));
         }
+
+        [Test]
+        public void ThenCanReadIndividualResultsFromScenarioOutline_AllPass_ShouldBeTestResultPassed()
+        {
+          var results = ParseResultsFile();
+          results.SetExampleSignatureBuilder(new xUnitExampleSignatureBuilder());
+
+          var feature = new Feature { Name = "Scenario Outlines" };
+
+          var scenarioOutline = new ScenarioOutline { Name = "This is a scenario outline where all scenarios pass", Feature = feature };
+
+          TestResult exampleResultOutline = results.GetScenarioOutlineResult(scenarioOutline);
+          exampleResultOutline.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult1 = results.GetExampleResult(scenarioOutline, new[] { "pass_1" });
+          exampleResult1.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult2 = results.GetExampleResult(scenarioOutline, new[] { "pass_2" });
+          exampleResult2.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult3 = results.GetExampleResult(scenarioOutline, new[] { "pass_3" });
+          exampleResult3.ShouldEqual(TestResult.Passed);
+        }
+
+        [Test]
+        public void ThenCanReadIndividualResultsFromScenarioOutline_OneFailed_ShouldBeTestResultFailed_xUnitDoesNotSupportInconclusive()
+        {
+          var results = ParseResultsFile();
+          results.SetExampleSignatureBuilder(new xUnitExampleSignatureBuilder());
+
+          var feature = new Feature { Name = "Scenario Outlines" };
+
+          var scenarioOutline = new ScenarioOutline { Name = "This is a scenario outline where one scenario is inconclusive", Feature = feature };
+
+          TestResult exampleResultOutline = results.GetScenarioOutlineResult(scenarioOutline);
+          exampleResultOutline.ShouldEqual(TestResult.Failed);
+
+          TestResult exampleResult1 = results.GetExampleResult(scenarioOutline, new[] { "pass_1" });
+          exampleResult1.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult2 = results.GetExampleResult(scenarioOutline, new[] { "pass_2" });
+          exampleResult2.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult3 = results.GetExampleResult(scenarioOutline, new[] { "inconclusive_1" });
+          exampleResult3.ShouldEqual(TestResult.Failed);
+        }
+
+        [Test]
+        public void ThenCanReadIndividualResultsFromScenarioOutline_OneFailed_ShouldBeTestResultFailed()
+        {
+          var results = ParseResultsFile();
+          results.SetExampleSignatureBuilder(new xUnitExampleSignatureBuilder());
+
+          var feature = new Feature { Name = "Scenario Outlines" };
+
+          var scenarioOutline = new ScenarioOutline { Name = "This is a scenario outline where one scenario fails", Feature = feature };
+
+          TestResult exampleResultOutline = results.GetScenarioOutlineResult(scenarioOutline);
+          exampleResultOutline.ShouldEqual(TestResult.Failed);
+
+          TestResult exampleResult1 = results.GetExampleResult(scenarioOutline, new[] { "pass_1" });
+          exampleResult1.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult2 = results.GetExampleResult(scenarioOutline, new[] { "pass_2" });
+          exampleResult2.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult3 = results.GetExampleResult(scenarioOutline, new[] { "fail_1" });
+          exampleResult3.ShouldEqual(TestResult.Failed);
+        }
+
+        [Test]
+        public void ThenCanReadIndividualResultsFromScenarioOutline_MultipleExampleSections_ShouldBeTestResultFailed()
+        {
+          var results = ParseResultsFile();
+          results.SetExampleSignatureBuilder(new xUnitExampleSignatureBuilder());
+
+          var feature = new Feature { Name = "Scenario Outlines" };
+
+          var scenarioOutline = new ScenarioOutline { Name = "And we can go totally bonkers with multiple example sections.", Feature = feature };
+
+          TestResult exampleResultOutline = results.GetScenarioOutlineResult(scenarioOutline);
+          exampleResultOutline.ShouldEqual(TestResult.Failed);
+
+          TestResult exampleResult1 = results.GetExampleResult(scenarioOutline, new[] { "pass_1" });
+          exampleResult1.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult2 = results.GetExampleResult(scenarioOutline, new[] { "pass_2" });
+          exampleResult2.ShouldEqual(TestResult.Passed);
+
+          TestResult exampleResult3 = results.GetExampleResult(scenarioOutline, new[] { "inconclusive_1" });
+          exampleResult3.ShouldEqual(TestResult.Failed);
+
+          TestResult exampleResult4 = results.GetExampleResult(scenarioOutline, new[] { "inconclusive_2" });
+          exampleResult4.ShouldEqual(TestResult.Failed);
+
+          TestResult exampleResult5 = results.GetExampleResult(scenarioOutline, new[] { "fail_1" });
+          exampleResult5.ShouldEqual(TestResult.Failed);
+
+          TestResult exampleResult6 = results.GetExampleResult(scenarioOutline, new[] { "fail_2" });
+          exampleResult6.ShouldEqual(TestResult.Failed);
+        }
     }
-}
+  }
