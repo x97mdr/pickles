c48af0fbc1d42efeecfc405ce3a58bcfd92e0767
Tom Davis <tom.davis@photomoose.com>
2014-06-09 22:18:20 +0100

    Fixed issue where multiple results files for a single feature caused exceptions when processing scenario outline examples.

---
+++ src/Pickles/Pickles.Test/WhenParsingMultipleNUnitResultsFilesForASingleFeature.cs
@@ -0,0 +1,72 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="WhenParsingMultipleNUnitResultsFiles.cs" company="PicklesDoc">
+//  Copyright 2011 Jeffrey Cameron
+//  Copyright 2012-present PicklesDoc team and community contributors
+//  
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using NUnit.Framework;
+using PicklesDoc.Pickles.ObjectModel;
+using PicklesDoc.Pickles.TestFrameworks;
+using Should;
+
+namespace PicklesDoc.Pickles.Test
+{
+    [TestFixture]
+    public class WhenParsingMultipleNUnitResultsFilesForASingleFeature : WhenParsingTestResultFiles<NUnitResults>
+    {
+        public WhenParsingMultipleNUnitResultsFilesForASingleFeature()
+            : base(@"results-example-nunit - Run 1.xml;results-example-nunit - Run 2.xml")
+        {
+        }
+
+        [Test]
+        public void ThenCanReadFeatureResultSuccessfully()
+        {
+            // Write out the embedded test results file
+            var results = ParseResultsFile();
+
+            var feature = new Feature { Name = "FeatureWithMultipleResultsFiles" };
+            TestResult result = results.GetFeatureResult(feature);
+
+            result.WasExecuted.ShouldBeTrue();
+            result.WasSuccessful.ShouldBeTrue();
+        }
+
+        [Test]
+        public void ThenCanReadScenarioOutlineResultSuccessfully()
+        {
+            var results = ParseResultsFile();
+
+            var feature = new Feature { Name = "FeatureWithMultipleResultsFiles" };
+            var scenarioOutline = new ScenarioOutline { Name = "Some scenario outline", Feature = feature };
+            
+            TestResult result = results.GetScenarioOutlineResult(scenarioOutline);
+
+            result.WasExecuted.ShouldBeTrue();
+            result.WasSuccessful.ShouldBeTrue();
+
+            TestResult exampleResult1 = results.GetExampleResult(scenarioOutline, new[] { "false" });
+            exampleResult1.WasExecuted.ShouldBeTrue();
+            exampleResult1.WasSuccessful.ShouldBeTrue();
+
+            TestResult exampleResult2 = results.GetExampleResult(scenarioOutline, new[] { "true" });
+            exampleResult2.WasExecuted.ShouldBeTrue();
+            exampleResult2.WasSuccessful.ShouldBeTrue();
+        }
+        
+    }
+}
