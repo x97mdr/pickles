a8036ba996fa77333e64550faaaa14d9c1e69aa7
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-09-15 15:28:10 +0200

    Inlining methods

--- src/Pickles/Pickles.Test/AssertExtensions.cs
+++ src/Pickles/Pickles.Test/AssertExtensions.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Xml.Linq;
 using NFluent;
 using NFluent.Extensibility;
-using NUnit.Framework;
 using PicklesDoc.Pickles.Test.Extensions;
 
 namespace PicklesDoc.Pickles.Test
@@ -14,51 +13,31 @@ namespace PicklesDoc.Pickles.Test
         {
             var actual = ExtensibilityHelper.ExtractChecker(check).Value;
 
-            ShouldHaveAttribute(actual, name, value);
-        }
-
-        private static void ShouldHaveAttribute(this XElement element, string name, string value)
-        {
-            XAttribute xAttribute = element.Attributes().FirstOrDefault(attribute => attribute.Name.LocalName == name);
-            Check.That(xAttribute).IsNotNull();
-            // ReSharper disable once PossibleNullReferenceException
-            Check.That(xAttribute.Value).IsEqualTo(value);
+          XAttribute xAttribute = actual.Attributes().FirstOrDefault(attribute => attribute.Name.LocalName == name);
+          Check.That(xAttribute).IsNotNull();
+          // ReSharper disable once PossibleNullReferenceException
+          Check.That(xAttribute.Value).IsEqualTo(value);
         }
 
         public static void HasElement(this ICheck<XElement> check, string name)
-        {
-          var actual = ExtensibilityHelper.ExtractChecker(check).Value;
+          {
+            var actual = ExtensibilityHelper.ExtractChecker(check).Value;
 
-          ShouldHaveElement(actual, name);
-        }
-
-        private static void ShouldHaveElement(this XElement element, string name)
-        {
-            Check.That(element.HasElement(name)).IsTrue();
-        }
+            Check.That(actual.HasElement(name)).IsTrue();
+          }
 
         public static void IsInNamespace(this ICheck<XElement> check, string nameOfNamespace)
         {
           var actual = ExtensibilityHelper.ExtractChecker(check).Value;
 
-          ShouldBeInNamespace(actual, nameOfNamespace);
-        }
-
-        private static void ShouldBeInNamespace(this XElement element, string nameOfNamespace)
-        {
-            Check.That(element.Name.NamespaceName).IsEqualTo(nameOfNamespace);
+          Check.That(actual.Name.NamespaceName).IsEqualTo(nameOfNamespace);
         }
 
         public static void IsNamed(this ICheck<XElement> check, string name)
         {
           var actual = ExtensibilityHelper.ExtractChecker(check).Value;
 
-          ShouldBeNamed(actual, name);
-        }
-
-        private static void ShouldBeNamed(this XElement element, string name)
-        {
-          Check.That(element.Name.LocalName).IsEqualTo(name);
+          Check.That(actual.Name.LocalName).IsEqualTo(name);
         }
 
         public static void IsDeeplyEqualTo(this ICheck<XElement> check, XElement actual)
