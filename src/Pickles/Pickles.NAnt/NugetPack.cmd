8bb3f136cefc5d77f91fe8d3beebda717a652752
Dirk Rombauts <dirk.rombauts@questmaster.net>
2014-12-02 17:47:37 +0100

    Delete nAnt source code

--- src/Pickles/Pickles.NAnt/NugetPack.cmd
+++
@@ -1,1 +0,0 @@
-nuget pack Pickles.NAnt.csproj -Properties Configuration=Release
--- src/Pickles/Pickles.NAnt/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Pickles.NAnt")]
-[assembly: AssemblyDescription("NAnt tasks for Pickles")]
