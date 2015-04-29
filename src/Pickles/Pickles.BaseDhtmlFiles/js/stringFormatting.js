56ab9590b2893314435098c924cef52b91b2d564
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-04-30 02:01:54 +0300

    Fix abbreviation parsing in folder names (#192)

--- src/Pickles/Pickles.BaseDhtmlFiles/js/stringFormatting.js
+++ src/Pickles/Pickles.BaseDhtmlFiles/js/stringFormatting.js
@@ -6,8 +6,8 @@
             return this.replace(/^\s+|\s+$/g, '');
         }
     }
-    
-    return unformattedString.replace(/([A-Z])/g, " $1").trim();
+
+    return unformattedString.replace(/([A-Z][a-z])/g, " $1").replace(/([A-Z]([A-Z])*)/g, " $1").trim();
 }
 
 function removeBeginningHash(hashedString) {
