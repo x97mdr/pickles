c31e95cf1f10004e3ea958f24851a1c10879a35c
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-05-02 12:08:21 +0200

    First Tests for TableCell

---
+++ src/Pickles/Pickles/ObjectModel/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PicklesDoc.Pickles.ObjectModel
+{
+    public class Mapper
+    {
+        static Mapper()
+        {
+            AutoMapper.Mapper.CreateMap<Gherkin3.Ast.TableCell, string>().ConstructUsing(cell => cell.Value);
+        }
+
+        public string Map(Gherkin3.Ast.TableCell cell)
+        {
+            return AutoMapper.Mapper.Map<Gherkin3.Ast.TableCell, string>(cell);
+        }
+    }
+}
