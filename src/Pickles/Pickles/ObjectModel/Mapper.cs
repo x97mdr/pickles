5733f8dff4490d638c91219168b4eafe7e577cbe
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-05-03 06:47:35 +0200

    Add namespace alias for Gherkin3.Ast

--- src/Pickles/Pickles/ObjectModel/Mapper.cs
+++ src/Pickles/Pickles/ObjectModel/Mapper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using G = Gherkin3.Ast;
 
 namespace PicklesDoc.Pickles.ObjectModel
 {
@@ -8,13 +9,13 @@ namespace PicklesDoc.Pickles.ObjectModel
     {
         static Mapper()
         {
-            AutoMapper.Mapper.CreateMap<Gherkin3.Ast.TableCell, string>().ConstructUsing(cell => cell.Value);
-            AutoMapper.Mapper.CreateMap<Gherkin3.Ast.TableRow, TableRow>()
+            AutoMapper.Mapper.CreateMap<G.TableCell, string>().ConstructUsing(cell => cell.Value);
+            AutoMapper.Mapper.CreateMap<G.TableRow, TableRow>()
                 .ConstructUsing(
                     row =>
                         new TableRow(
                             row.Cells.Select(AutoMapper.Mapper.Map<string>)));
-            AutoMapper.Mapper.CreateMap<Gherkin3.Ast.DataTable, Table>()
+            AutoMapper.Mapper.CreateMap<G.DataTable, Table>()
                 .ConstructUsing(
                     dataTable =>
                         new Table
@@ -24,22 +25,22 @@ namespace PicklesDoc.Pickles.ObjectModel
                         });
         }
 
-        public string MapToString(Gherkin3.Ast.TableCell cell)
+        public string MapToString(G.TableCell cell)
         {
             return AutoMapper.Mapper.Map<string>(cell);
         }
 
-        public TableRow MapToTableRow(Gherkin3.Ast.TableRow tableRow)
+        public TableRow MapToTableRow(G.TableRow tableRow)
         {
             return AutoMapper.Mapper.Map<TableRow>(tableRow);
         }
 
-        public Table MapToTable(Gherkin3.Ast.DataTable dataTable)
+        public Table MapToTable(G.DataTable dataTable)
         {
             return AutoMapper.Mapper.Map<Table>(dataTable);
         }
 
-        public string MapToString(Gherkin3.Ast.DocString docString)
+        public string MapToString(G.DocString docString)
         {
             return AutoMapper.Mapper.Map<string>(docString);
         }
