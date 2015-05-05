4a384069801d26706afaaf95f492bc0145215e1f
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-05-05 16:02:46 +0200

    First Test for Scenario mapping

---
+++ src/Pickles/Pickles.Test/ObjectModel/MapperTestsForScenario.cs
@@ -0,0 +1,44 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="MApperTestsForScenario.cs" company="PicklesDoc">
+//  Copyright 2011 Jeffrey Cameron
+//  Copyright 2012-present PicklesDoc team and community contributors
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using NFluent;
+using NUnit.Framework;
+using PicklesDoc.Pickles.ObjectModel;
+using G = Gherkin3.Ast;
+
+namespace PicklesDoc.Pickles.Test.ObjectModel
+{
+    [TestFixture]
+    public class MapperTestsForScenario
+    {
+        private readonly Factory factory = new Factory();
+
+        [Test]
+        public void MapScenario_NullScenario_ReturnsNull()
+        {
+            var mapper = this.factory.CreateMapper();
+
+            Scenario result = mapper.MapToScenario((G.Scenario) null);
+
+            Check.That(result).IsNull();
+        }
+    }
+}
