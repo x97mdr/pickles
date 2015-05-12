2efb639222b9f98685bc3a784b346778067dc857
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-05-12 16:03:58 +0200

    Version bump (1.1.0)

--- src/Pickles/VersionInfo.cs
+++ src/Pickles/VersionInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.0.1")]
-[assembly: AssemblyFileVersionAttribute("1.0.1")]
+[assembly: AssemblyVersionAttribute("1.1.0")]
+[assembly: AssemblyFileVersionAttribute("1.1.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.1";
+        internal const string Version = "1.1.0";
     }
 }
