6d6a6cb5a83c264f135163b964dc9f29c86157e1
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-05-16 17:49:48 +0200

    Use an instance version of the mapping configuration

--- src/Pickles/Pickles/ObjectModel/Mapper.cs
+++ src/Pickles/Pickles/ObjectModel/Mapper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
+using AutoMapper.Mappers;
 using G = Gherkin3.Ast;
 
 namespace PicklesDoc.Pickles.ObjectModel
@@ -8,132 +10,139 @@ namespace PicklesDoc.Pickles.ObjectModel
     public class Mapper
     {
         private readonly string featureLanguage;
+        private MappingEngine mapper;
 
         static Mapper()
         {
-            AutoMapper.Mapper.CreateMap<G.TableCell, string>()
-                .ConstructUsing(cell => cell.Value);
-
-            AutoMapper.Mapper.CreateMap<G.TableRow, TableRow>()
-                .ConstructUsing(row => new TableRow(row.Cells.Select(AutoMapper.Mapper.Map<string>)));
-
-            AutoMapper.Mapper.CreateMap<G.DataTable, Table>()
-                .ForMember(t => t.HeaderRow, opt => opt.MapFrom(dt => dt.Rows.Take(1).Single()))
-                .ForMember(t => t.DataRows, opt => opt.MapFrom(dt => dt.Rows.Skip(1)));
-
-            AutoMapper.Mapper.CreateMap<G.DocString, string>().ConstructUsing(docString => docString.Content);
-
-            AutoMapper.Mapper.CreateMap<G.Step, Step>()
-                .ForMember(t => t.NativeKeyword, opt => opt.MapFrom(s => s.Keyword))
-                .ForMember(t => t.Name, opt => opt.MapFrom(s => s.Text))
-                .ForMember(t => t.Keyword, opt => opt.MapFrom(s => s.Keyword))
-                .ForMember(t => t.DocStringArgument, opt => opt.MapFrom(s => s.Argument is G.DocString ? s.Argument : null))
-                .ForMember(t => t.TableArgument, opt => opt.MapFrom(s => s.Argument is G.DataTable ? s.Argument : null));
-
-            AutoMapper.Mapper.CreateMap<G.Tag, string>()
-                .ConstructUsing(tag => tag.Name);
-
-            AutoMapper.Mapper.CreateMap<G.Scenario, Scenario>()
-                .ForMember(t => t.Description, opt => opt.NullSubstitute(string.Empty));
-
-            AutoMapper.Mapper.CreateMap<IEnumerable<G.TableRow>, Table>()
-                .ForMember(t => t.HeaderRow, opt => opt.MapFrom(s => s.Take(1).Single()))
-                .ForMember(t => t.DataRows, opt => opt.MapFrom(s => s.Skip(1)));
-
-            AutoMapper.Mapper.CreateMap<G.Examples, Example>()
-                .ForMember(t => t.TableArgument, opt => opt.MapFrom(s => ((G.IHasRows) s).Rows));
-
-            AutoMapper.Mapper.CreateMap<G.ScenarioOutline, ScenarioOutline>()
-                .ForMember(t => t.Description, opt => opt.NullSubstitute(string.Empty));
-
-            AutoMapper.Mapper.CreateMap<G.Background, Scenario>()
-                .ForMember(t => t.Description, opt => opt.NullSubstitute(string.Empty));
-
-            AutoMapper.Mapper.CreateMap<G.ScenarioDefinition, IFeatureElement>().ConvertUsing(
-                sd =>
-                {
-                    var scenario = sd as G.Scenario;
-                    if (scenario != null)
-                    {
-                        return AutoMapper.Mapper.Map<Scenario>(scenario);
-                    }
-
-                    var scenarioOutline = sd as G.ScenarioOutline;
-                    if (scenarioOutline != null)
-                    {
-                        return AutoMapper.Mapper.Map<ScenarioOutline>(scenarioOutline);
-                    }
-
-                    throw new ArgumentException("Only arguments of type Scenario and ScenarioOutline are supported.");
-                });
-
-            AutoMapper.Mapper.CreateMap<G.Feature, Feature>()
-                .ForMember(t => t.FeatureElements, opt => opt.ResolveUsing(s => s.ScenarioDefinitions));
         }
 
         public Mapper(string featureLanguage = "en")
         {
             this.featureLanguage = featureLanguage;
+
+            var configurationStore = new ConfigurationStore(new TypeMapFactory(), MapperRegistry.Mappers);
+
+            this.mapper = new MappingEngine(configurationStore);
+
+            configurationStore.CreateMap<string, Keyword>().ConvertUsing(new KeywordResolver(this.featureLanguage));
+
+            configurationStore.CreateMap<G.TableCell, string>()
+        .ConstructUsing(cell => cell.Value);
+
+            configurationStore.CreateMap<G.TableRow, TableRow>()
+                .ConstructUsing(row => new TableRow(row.Cells.Select(this.mapper.Map<string>)));
+
+            configurationStore.CreateMap<G.DataTable, Table>()
+                .ForMember(t => t.HeaderRow, opt => opt.MapFrom(dt => dt.Rows.Take(1).Single()))
+                .ForMember(t => t.DataRows, opt => opt.MapFrom(dt => dt.Rows.Skip(1)));
+
+            configurationStore.CreateMap<G.DocString, string>().ConstructUsing(docString => docString.Content);
+
+            configurationStore.CreateMap<G.Step, Step>()
+                .ForMember(t => t.NativeKeyword, opt => opt.MapFrom(s => s.Keyword))
+                .ForMember(t => t.Name, opt => opt.MapFrom(s => s.Text))
+                .ForMember(t => t.DocStringArgument, opt => opt.MapFrom(s => s.Argument is G.DocString ? s.Argument : null))
+                .ForMember(t => t.TableArgument, opt => opt.MapFrom(s => s.Argument is G.DataTable ? s.Argument : null));
+
+            configurationStore.CreateMap<G.Tag, string>()
+                .ConstructUsing(tag => tag.Name);
+
+            configurationStore.CreateMap<G.Scenario, Scenario>()
+                .ForMember(t => t.Description, opt => opt.NullSubstitute(string.Empty));
+
+            configurationStore.CreateMap<IEnumerable<G.TableRow>, Table>()
+                .ForMember(t => t.HeaderRow, opt => opt.MapFrom(s => s.Take(1).Single()))
+                .ForMember(t => t.DataRows, opt => opt.MapFrom(s => s.Skip(1)));
+
+            configurationStore.CreateMap<G.Examples, Example>()
+                .ForMember(t => t.TableArgument, opt => opt.MapFrom(s => ((G.IHasRows)s).Rows));
+
+            configurationStore.CreateMap<G.ScenarioOutline, ScenarioOutline>()
+                .ForMember(t => t.Description, opt => opt.NullSubstitute(string.Empty));
+
+            configurationStore.CreateMap<G.Background, Scenario>()
+                .ForMember(t => t.Description, opt => opt.NullSubstitute(string.Empty));
+
+            configurationStore.CreateMap<G.ScenarioDefinition, IFeatureElement>().ConvertUsing(
+                sd =>
+                {
+                    var scenario = sd as G.Scenario;
+                    if (scenario != null)
+                    {
+                        return this.mapper.Map<Scenario>(scenario);
+                    }
+
+                    var scenarioOutline = sd as G.ScenarioOutline;
+                    if (scenarioOutline != null)
+                    {
+                        return this.mapper.Map<ScenarioOutline>(scenarioOutline);
+                    }
+
+                    throw new ArgumentException("Only arguments of type Scenario and ScenarioOutline are supported.");
+                });
+
+            configurationStore.CreateMap<G.Feature, Feature>()
+                .ForMember(t => t.FeatureElements, opt => opt.ResolveUsing(s => s.ScenarioDefinitions));
         }
 
         public string MapToString(G.TableCell cell)
         {
-            return AutoMapper.Mapper.Map<string>(cell);
+            return this.mapper.Map<string>(cell);
         }
 
         public TableRow MapToTableRow(G.TableRow tableRow)
         {
-            return AutoMapper.Mapper.Map<TableRow>(tableRow);
+            return this.mapper.Map<TableRow>(tableRow);
         }
 
         public Table MapToTable(G.DataTable dataTable)
         {
-            return AutoMapper.Mapper.Map<Table>(dataTable);
+            return this.mapper.Map<Table>(dataTable);
         }
 
         public string MapToString(G.DocString docString)
         {
-            return AutoMapper.Mapper.Map<string>(docString);
+            return this.mapper.Map<string>(docString);
         }
 
         public Step MapToStep(G.Step step)
         {
-            return AutoMapper.Mapper.Map<Step>(step);
+            return this.mapper.Map<Step>(step);
         }
 
         public Keyword MapToKeyword(string keyword)
         {
-            return AutoMapper.Mapper.Map<Keyword>(keyword);
+            return this.mapper.Map<Keyword>(keyword);
         }
 
         public string MapToString(G.Tag tag)
         {
-            return AutoMapper.Mapper.Map<string>(tag);
+            return this.mapper.Map<string>(tag);
         }
 
         public Scenario MapToScenario(G.Scenario scenario)
         {
-            return AutoMapper.Mapper.Map<Scenario>(scenario);
+            return this.mapper.Map<Scenario>(scenario);
         }
 
         public Example MapToExample(G.Examples examples)
         {
-            return AutoMapper.Mapper.Map<Example>(examples);
+            return this.mapper.Map<Example>(examples);
         }
 
         public ScenarioOutline MapToScenarioOutline(G.ScenarioOutline scenarioOutline)
         {
-            return AutoMapper.Mapper.Map<ScenarioOutline>(scenarioOutline);
+            return this.mapper.Map<ScenarioOutline>(scenarioOutline);
         }
 
         public Scenario MapToScenario(G.Background background)
         {
-            return AutoMapper.Mapper.Map<Scenario>(background);
+            return this.mapper.Map<Scenario>(background);
         }
 
         public Feature MapToFeature(G.Feature feature)
         {
-            return AutoMapper.Mapper.Map<Feature>(feature);
+            return this.mapper.Map<Feature>(feature);
         }
     }
 }
