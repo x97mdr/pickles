1fca9b25685b5a8e84e2801a9dfd1acd8f1d1763
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-07-23 10:34:49 +0200

    Add test for failing parsing of xUnit 2

---
+++ src/Pickles/Pickles.Test/TestFrameworks/WhenParsingxUnit2ResultsFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using PicklesDoc.Pickles.ObjectModel;
+using PicklesDoc.Pickles.TestFrameworks;
+
+using NFluent;
+
+namespace PicklesDoc.Pickles.Test.TestFrameworks
+{
+    [TestFixture]
+    public class WhenParsingxUnit2ResultsFile : WhenParsingTestResultFiles<XUnitResults>
+    {
+        public WhenParsingxUnit2ResultsFile()
+            : base("results-example-xunit2.xml")
+        {
+        }
+
+        [Test]
+        public void ThenCannotReadFeatureResult()
+        {
+            // Write out the embedded test results file
+            var results = ParseResultsFile();
+
+            var feature = new Feature { Name = "Addition" };
+
+            Check.ThatCode(() => results.GetFeatureResult(feature)).Throws<Exception>();
+        }
+    }
+  }
