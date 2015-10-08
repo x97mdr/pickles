fa000766ba8667f70e566dfc866c1915a5120ff8
Dirk Rombauts <dirk.rombauts@questmaster.net>
2015-10-08 12:36:02 +0200

    Move tests to their logical location.

--- src/Pickles/Pickles.Test/Extensions/StringExtensionsTests.cs
+++ src/Pickles/Pickles.Test/Extensions/StringExtensionsTests.cs
@@ -28,5 +28,53 @@ namespace PicklesDoc.Pickles.Test.Extensions
     [TestFixture]
     public class StringExtensionsTests
     {
+        [Test]
+        public void ThenCanFormatTextAsWikiWordSuccessfully()
+        {
+            string actual = "ThisIsTheWikiWord".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("This Is The Wiki Word");
+        }
+
+        [Test]
+        public void ThenCanFormatTextWithAcronymAndNumberAsWikiWordSuccessfully()
+        {
+            string actual = "ThisIsAnACRONYM1".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("This Is An ACRONYM1");
+        }
+
+        [Test]
+        public void ThenCanFormatTextWithAcronymAsWikiWordSuccessfully()
+        {
+            string actual = "ThisIsAnACRONYM".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("This Is An ACRONYM");
+        }
+
+        [Test]
+        public void ThenCanFormatTextWithLongNumbersAsWikiWordSuccessfully()
+        {
+            string actual = "ThisIsThe5000thWikiWord".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("This Is The 5000th Wiki Word");
+        }
+
+        [Test]
+        public void ThenCanFormatTextWithNumberFollowedByCapitalAsWikiWordSuccessfully()
+        {
+            string actual = "001FeatureOne".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("001 Feature One");
+        }
+
+        [Test]
+        public void ThenCanFormatTextWithNumbersAsWikiWordSuccessfully()
+        {
+            string actual = "ThisIsThe4thWikiWord".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("This Is The 4th Wiki Word");
+        }
+
+        [Test]
+        public void ThenCanFormatTextWithSpecialCharactersAsWikiWordSuccessfully()
+        {
+            string actual = "ThisIsThe_WikiWord".ExpandWikiWord();
+            Check.That(actual).IsEqualTo("This Is The Wiki Word");
+        }
     }
 }
--- src/Pickles/Pickles.Test/WhenFormattingWikiWords.cs
+++ src/Pickles/Pickles.Test/WhenFormattingWikiWords.cs
@@ -28,53 +28,5 @@ namespace PicklesDoc.Pickles.Test
     [TestFixture]
     public class WhenFormattingWikiWords : BaseFixture
     {
-        [Test]
-        public void ThenCanFormatTextAsWikiWordSuccessfully()
-        {
-            string actual = "ThisIsTheWikiWord".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("This Is The Wiki Word");
-        }
-
-        [Test]
-        public void ThenCanFormatTextWithAcronymAndNumberAsWikiWordSuccessfully()
-        {
-            string actual = "ThisIsAnACRONYM1".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("This Is An ACRONYM1");
-        }
-
-        [Test]
-        public void ThenCanFormatTextWithAcronymAsWikiWordSuccessfully()
-        {
-            string actual = "ThisIsAnACRONYM".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("This Is An ACRONYM");
-        }
-
-        [Test]
-        public void ThenCanFormatTextWithLongNumbersAsWikiWordSuccessfully()
-        {
-            string actual = "ThisIsThe5000thWikiWord".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("This Is The 5000th Wiki Word");
-        }
-
-        [Test]
-        public void ThenCanFormatTextWithNumberFollowedByCapitalAsWikiWordSuccessfully()
-        {
-            string actual = "001FeatureOne".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("001 Feature One");
-        }
-
-        [Test]
-        public void ThenCanFormatTextWithNumbersAsWikiWordSuccessfully()
-        {
-            string actual = "ThisIsThe4thWikiWord".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("This Is The 4th Wiki Word");
-        }
-
-        [Test]
-        public void ThenCanFormatTextWithSpecialCharactersAsWikiWordSuccessfully()
-        {
-            string actual = "ThisIsThe_WikiWord".ExpandWikiWord();
-            Check.That(actual).IsEqualTo("This Is The Wiki Word");
-        }
     }
 }
