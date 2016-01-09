b31b8085e3cdddb9de3459cfe1e4fd20f8a2bb6e
Dirk Rombauts <dirk.rombauts@questmaster.net>
2016-01-09 18:37:02 +0100

    Add infrastructure for XUnit2

---
+++ src/Pickles/Pickles.TestFrameworks/XUnit2/XUnit2SingleResults.cs
@@ -0,0 +1,56 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="XUnit2SingleResults.cs" company="PicklesDoc">
+//  Copyright 2011 Jeffrey Cameron
+//  Copyright 2012-present PicklesDoc team and community contributors
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+using PicklesDoc.Pickles.ObjectModel;
+
+namespace PicklesDoc.Pickles.TestFrameworks.XUnit2
+{
+    public class XUnit2SingleResults : ITestResults
+    {
+        public bool SupportsExampleResults
+        {
+            get { return true; }
+        }
+
+        internal XUnit2ExampleSignatureBuilder ExampleSignatureBuilder { get; set; }
+
+        public TestResult GetFeatureResult(Feature feature)
+        {
+        }
+
+        public TestResult GetScenarioOutlineResult(ScenarioOutline scenarioOutline)
+        {
+        }
+
+        public TestResult GetScenarioResult(Scenario scenario)
+        {
+        }
+
+        public TestResult GetExampleResult(ScenarioOutline scenarioOutline, string[] exampleValues)
+        {
+        }
+    }
+}
