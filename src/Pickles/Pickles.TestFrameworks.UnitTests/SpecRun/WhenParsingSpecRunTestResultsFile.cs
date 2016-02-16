0081b51fbd093b787fc47958b23f3fad12c07105
Dirk Rombauts <dirk.rombauts@questmaster.net>
2016-02-16 13:58:45 +0100

    Use standard test suite in SpecRun

--- src/Pickles/Pickles.TestFrameworks.UnitTests/SpecRun/WhenParsingSpecRunTestResultsFile.cs
+++ src/Pickles/Pickles.TestFrameworks.UnitTests/SpecRun/WhenParsingSpecRunTestResultsFile.cs
@@ -20,17 +20,14 @@
 
 using System;
 
-using NFluent;
-
 using NUnit.Framework;
 
-using PicklesDoc.Pickles.ObjectModel;
 using PicklesDoc.Pickles.TestFrameworks.SpecRun;
 
 namespace PicklesDoc.Pickles.TestFrameworks.UnitTests.SpecRun
 {
     [TestFixture]
-    public class WhenParsingSpecRunTestResultsFile : WhenParsingTestResultFiles<SpecRunResults>
+    public class WhenParsingSpecRunTestResultsFile : StandardTestSuite<SpecRunResults>
     {
         public WhenParsingSpecRunTestResultsFile()
             : base("SpecRun." + "results-example-specrun.html")
@@ -38,125 +35,57 @@ namespace PicklesDoc.Pickles.TestFrameworks.UnitTests.SpecRun
         }
 
         [Test]
-        public void ThenCanReadBackgroundResultSuccessfully()
+        public new void ThenCanReadBackgroundResultSuccessfully()
         {
-            var background = new Scenario { Name = "Background", Feature = this.AdditionFeature() };
-            var feature = this.AdditionFeature();
-            feature.AddBackground(background);
-            var results = ParseResultsFile();
-
-            TestResult result = results.GetScenarioResult(background);
-
-            Check.That(result).IsEqualTo(TestResult.Inconclusive);
+            base.ThenCanReadBackgroundResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadInconclusiveFeatureResultSuccessfully()
+        public new void ThenCanReadInconclusiveFeatureResultSuccessfully()
         {
-            var results = ParseResultsFile();
-
-            TestResult result = results.GetFeatureResult(this.InconclusiveFeature());
-
-            Check.That(result).IsEqualTo(TestResult.Inconclusive);
+            base.ThenCanReadInconclusiveFeatureResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadFailedFeatureResultSuccessfully()
+        public new void ThenCanReadFailedFeatureResultSuccessfully()
         {
-            var results = ParseResultsFile();
-
-            TestResult result = results.GetFeatureResult(this.FailingFeature());
-
-            Check.That(result).IsEqualTo(TestResult.Failed);
+            base.ThenCanReadFailedFeatureResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadPassedFeatureResultSuccessfully()
+        public new void ThenCanReadPassedFeatureResultSuccessfully()
         {
-            var results = ParseResultsFile();
-
-            TestResult result = results.GetFeatureResult(this.PassingFeature());
-
-            Check.That(result).IsEqualTo(TestResult.Passed);
+            base.ThenCanReadPassedFeatureResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadScenarioOutlineResultSuccessfully()
+        public new void ThenCanReadScenarioOutlineResultSuccessfully()
         {
-            var results = ParseResultsFile();
-            var scenarioOutline = new ScenarioOutline { Name = "Adding several numbers", Feature = this.AdditionFeature() };
-
-            TestResult result = results.GetScenarioOutlineResult(scenarioOutline);
-
-            Check.That(result).IsEqualTo(TestResult.Passed);
+            base.ThenCanReadScenarioOutlineResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadSuccessfulScenarioResultSuccessfully()
+        public new void ThenCanReadSuccessfulScenarioResultSuccessfully()
         {
-            var results = ParseResultsFile();
-            var passedScenario = new Scenario { Name = "Add two numbers", Feature = this.AdditionFeature() };
-
-            TestResult result = results.GetScenarioResult(passedScenario);
-
-            Check.That(result).IsEqualTo(TestResult.Passed);
+            base.ThenCanReadSuccessfulScenarioResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadFailedScenarioResultSuccessfully()
+        public new void ThenCanReadFailedScenarioResultSuccessfully()
         {
-            var results = ParseResultsFile();
-            var scenario = new Scenario { Name = "Fail to add two numbers", Feature = this.AdditionFeature() };
-            TestResult result = results.GetScenarioResult(scenario);
-
-            Check.That(result).IsEqualTo(TestResult.Failed);
+            base.ThenCanReadFailedScenarioResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadIgnoredScenarioResultSuccessfully()
+        public new void ThenCanReadIgnoredScenarioResultSuccessfully()
         {
-            var results = ParseResultsFile();
-            var ignoredScenario = new Scenario { Name = "Ignored adding two numbers", Feature = this.AdditionFeature() };
-
-            var result = results.GetScenarioResult(ignoredScenario);
-
-            Check.That(result).IsEqualTo(TestResult.Inconclusive);
+            base.ThenCanReadIgnoredScenarioResultSuccessfully();
         }
 
         [Test]
-        public void ThenCanReadInconclusiveScenarioResultSuccessfully()
+        public new void ThenCanReadInconclusiveScenarioResultSuccessfully()
         {
-            var results = ParseResultsFile();
-
-            var inconclusiveScenario = new Scenario
-            {
-                Name = "Not automated adding two numbers",
-                Feature = this.AdditionFeature()
-            };
-
-            var result = results.GetScenarioResult(inconclusiveScenario);
-
-            Check.That(result).IsEqualTo(TestResult.Inconclusive);
-        }
-
-        private Feature AdditionFeature()
-        {
-            return new Feature { Name = "Addition" };
-        }
-
-        private Feature InconclusiveFeature()
-        {
-            return new Feature { Name = "Inconclusive" };
-        }
-
-        private Feature FailingFeature()
-        {
-            return new Feature { Name = "Failing" };
-        }
-
-        private Feature PassingFeature()
-        {
-            return new Feature { Name = "Passing" };
+            base.ThenCanReadInconclusiveScenarioResultSuccessfully();
         }
     }
 }
