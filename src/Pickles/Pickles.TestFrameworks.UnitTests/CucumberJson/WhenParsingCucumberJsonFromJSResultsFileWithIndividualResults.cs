5966601a4883123e51f59faead8bb1693c6046f7
Dirk Rombauts <dirk.rombauts@questmaster.net>
2016-02-22 15:19:08 +0100

    Add results and tests for a JS version of cucumber

---
+++ src/Pickles/Pickles.TestFrameworks.UnitTests/CucumberJson/WhenParsingCucumberJsonFromJSResultsFileWithIndividualResults.cs
@@ -0,0 +1,61 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="WhenParsingCucumberJsonFromJSResultsFileWithIndividualResults.cs" company="PicklesDoc">
+//  Copyright 2011 Jeffrey Cameron
+//  Copyright 2012-present PicklesDoc team and community contributors
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+using NUnit.Framework;
+
+using PicklesDoc.Pickles.TestFrameworks.CucumberJson;
+
+namespace PicklesDoc.Pickles.TestFrameworks.UnitTests.CucumberJson
+{
+    [TestFixture]
+    public class WhenParsingCucumberJsonFromJSResultsFileWithIndividualResults : StandardTestSuiteForScenarioOutlines<CucumberJsonResults>
+    {
+        public WhenParsingCucumberJsonFromJSResultsFileWithIndividualResults()
+            : base("CucumberJson." + "results-example-cucumberjs-json.json")
+        {
+        }
+
+        [Test]
+        public new void ThenCanReadIndividualResultsFromScenarioOutline_AllPass_ShouldBeTestResultPassed()
+        {
+            base.ThenCanReadIndividualResultsFromScenarioOutline_AllPass_ShouldBeTestResultPassed();
+        }
+
+        [Test]
+        public new void ThenCanReadIndividualResultsFromScenarioOutline_OneInconclusive_ShouldBeTestResultInconclusive()
+        {
+            base.ThenCanReadIndividualResultsFromScenarioOutline_OneInconclusive_ShouldBeTestResultInconclusive();
+        }
+
+        [Test]
+        public new void ThenCanReadIndividualResultsFromScenarioOutline_OneFailed_ShouldBeTestResultFailed()
+        {
+            base.ThenCanReadIndividualResultsFromScenarioOutline_OneFailed_ShouldBeTestResultFailed();
+        }
+
+        [Test]
+        public new void ThenCanReadIndividualResultsFromScenarioOutline_MultipleExampleSections_ShouldBeTestResultFailed()
+        {
+            base.ThenCanReadIndividualResultsFromScenarioOutline_MultipleExampleSections_ShouldBeTestResultFailed();
+        }
+    }
+}
