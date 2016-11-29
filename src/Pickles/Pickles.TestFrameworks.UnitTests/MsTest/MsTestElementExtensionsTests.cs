0ce3dedba4b0533fd0f4c13620acb4dcb2026569
Dirk Rombauts <dirkrombauts@users.noreply.github.com>
2016-11-29 15:38:29 +0100

    Fix 356 (#398)
    * NCrunch v3 configuration
    
    * Add test to show problem
    
    * Fix problem

---
+++ src/Pickles/Pickles.TestFrameworks.UnitTests/MsTest/MsTestElementExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="MsTestElementExtensionsTests.cs" company="PicklesDoc">
+//  Copyright 2011 Jeffrey Cameron
+//  Copyright 2012-present PicklesDoc team and community contributors
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+using NFluent;
+
+using NUnit.Framework;
+
+using PicklesDoc.Pickles.TestFrameworks.MsTest;
+
+namespace PicklesDoc.Pickles.TestFrameworks.UnitTests.MsTest
+{
+    [TestFixture]
+    public class MsTestElementExtensionsTests
+    {
+        [Test]
+        public void ExecutionIdElement_NullArgument_ShouldReturnGuidEmpty()
+        {
+            var result = MsTestElementExtensions.ExecutionIdElement(null);
+
+            Check.That(result).IsEqualTo(Guid.Empty);
+        }
+    }
+}
